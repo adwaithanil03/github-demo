a3856e597cc3933dae1ec081235f2b5b441f3e55
adwaithanil03 <aniladwaith69@gmail.com>
2026-05-13 15:04:30 +0530

    Fix merge conflict in code.py
    Resolve merge conflict and update print statements.

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
 import mymodule 
 print("my first line")
 print("my second line")
-<<<<<<< HEAD
+
 print("my latest3 third line")
-=======
-print("my latest third3 line")
->>>>>>> week-client-changes
+print("next line")
+
